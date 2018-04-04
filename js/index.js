5f94a919e4f100d93a82b725f8e6ae1488f58349
Herdanu Yudistira Putra <heh.danu@gmail.com>
2018-04-04 14:00:18 +0700

    create JavaScript file to add todo list into input box

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const createListToDo = document.getElementById('createListToDo');
+const addButton = document.getElementById('addButton');
+const outputBox = document.getElementById('outputBox');
+const form = document.getElementById('form');
+let listInArray = localStorage.getItem('listToDo') ? JSON.parse(localStorage.getItem('listToDo')) : [];
+
+localStorage.setItem('listToDo', JSON.stringify(listInArray));
+const data = JSON.parse(localStorage.getItem('listToDo'));
+
+const listToDo = (text) => {
+  const div = document.createElement('div');
+  div.textContent = text;
+  outputBox.appendChild(div);
+}
+
+const createToDo = function(e) {
+  e.preventDefault();
+
+  listToDo(createListToDo.value);
+  listInArray.push(createListToDo.value);
+  localStorage.setItem('listToDo', JSON.stringify(listInArray));
+  createListToDo.value = '';
+};
+
+addButton.addEventListener('click', createToDo);
+form.addEventListener('submit', createToDo);
