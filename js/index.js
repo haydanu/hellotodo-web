7c1f84830e575434bda31b82b2e60b62be6a4848
Herdanu Yudistira Putra <heh.danu@gmail.com>
2018-04-05 11:10:32 +0700

    make input-box required

--- js/index.js
+++ js/index.js
@@ -2,14 +2,16 @@ const createListToDo = document.getElementById('createListToDo');
 const addButton = document.getElementById('addButton');
 const outputBox = document.getElementById('outputBox');
 const form = document.getElementById('form');
-let listInArray = localStorage.getItem('listToDo') ? JSON.parse(localStorage.getItem('listToDo')) : [];
+
+let listInArray = [];
+//localStorage.getItem('listToDo') ? JSON.parse(localStorage.getItem('listToDo')) : [];
 
 localStorage.setItem('listToDo', JSON.stringify(listInArray));
 const data = JSON.parse(localStorage.getItem('listToDo'));
 
 const listToDo = (text) => {
   const div = document.createElement('div');
-  div.textContent = text;
+  div.innerHTML = `${text} <i class='fa fa-remove'></i>`;
   outputBox.appendChild(div);
 }
 
@@ -21,6 +23,8 @@ const createToDo = function(e) {
   localStorage.setItem('listToDo', JSON.stringify(listInArray));
   createListToDo.value = '';
 };
+
+
 
 addButton.addEventListener('click', createToDo);
 form.addEventListener('submit', createToDo);
